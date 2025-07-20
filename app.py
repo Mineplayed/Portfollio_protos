1617ffd602a1059f975e0ed3f828a9c4ed18343b
Mineplayed <pierre.noe.mouchel@gmail.com>
2025-07-20 02:05:23 +0200

    cleaned app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def redirect_home():
+    return redirect("/home")
+    
+    
+@app.route("/home")
+def render_home():
     return render_template('index.html')
 
 if __name__ == '__main__':
